b3795b8bb1b9b00df96f0015a94f47a67c021ecf
Isaac <ISAACPEH.21@ichat.sp.edu.sg>
2023-07-11 20:40:38 +0800

    Test Cypress formManagement

--- cypress/e2e/formManagement.cy.js
+++ cypress/e2e/formManagement.cy.js
@@ -6,7 +6,7 @@ const randomName = chance.name()
 const minDate = new Date('2005-01-01'); // Define the minimum date
 const maxDate = new Date('2022-12-31'); // Define the maximum date
 
-const randomDate = chance.date({min: minDate, max: maxDate});
+const randomDate = chance.date({ min: minDate, max: maxDate });
 const formattedDate = randomDate.toISOString().substring(0, 10);
 const randomNumber = chance.integer({ min: 1, max: 147 });
 const nricPattern = /^[STFG]\d{7}[A-Z]$/i;
@@ -18,17 +18,17 @@ do {
 let authToken;
 
 before(() => {
-    const emailLogin = 'rltk4545@gomail.com';
-    const passLogin = 'PASSword1*';
-    cy.login(emailLogin, passLogin);
-    cy.getCookie('jwt').then((cookie) => {
-        authToken = cookie.value;
-    })
+  const emailLogin = 'rltk4545@gomail.com';
+  const passLogin = 'PASSword1*';
+  cy.doctorlogin(emailLogin, passLogin);
+  cy.getCookie('jwt').then((cookie) => {
+    authToken = cookie.value;
+  })
 });
 
 beforeEach(() => {
-    cy.setCookie('jwt', authToken);
-    cy.visit('http://localhost:3000/obs-form');
+  cy.setCookie('jwt', authToken);
+  cy.visit('http://localhost:3000/obs-form');
 });
 
 describe('fill in the form', () => {
@@ -38,6 +38,6 @@ describe('fill in the form', () => {
   // })
 
   it('should retrieve the correct doctor information', () => {
-    cy.fillInForm(randomName,formattedDate,randomNumber,randomNRIC);
+    cy.fillInForm(randomName, formattedDate, randomNumber, randomNRIC);
   })
 });
