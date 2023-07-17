ed1057230f4df236abf588b9d20e4d969445571d
isaacpeh <isaacpeh123@gmail.com>
2023-07-17 18:55:06 +0800

    Wip tooltips for buttons

--- public/obs-admin/profile/index.js
+++ public/obs-admin/profile/index.js
@@ -387,5 +387,12 @@ window.addEventListener('DOMContentLoaded', () => {
         e.preventDefault();
         togglePassword('enter-password-input', 'confirm-btn')
     }
+
+    // INITIALIZE BOOTSTRAP TOOLTIP
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl, { trigger: 'hover' });
+    });
+
 })
 
