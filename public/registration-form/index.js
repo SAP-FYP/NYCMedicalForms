5a3e7d34e9bf0a85c7f007dd8f4b68ceec4e8892
Isaac <ISAACPEH.21@ichat.sp.edu.sg>
2023-07-29 05:19:40 +0800

    Wip registration form

--- public/registration-form/index.js
+++ public/registration-form/index.js
@@ -58,8 +58,35 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    const displayNote = (callingEl, note, type) => {
+        const template = document.querySelector("#note-template");
+
+        if (note == "specialist memo") {
+            note = "A specialist memo is needed for further assessment. Complete this registration and get in touch with the Teacher Coordinator."
+        } else if (note == "physiotherapist memo") {
+            note = "A specialist/physiotherapist memo is needed for further assessment. Complete this registration and get in touch with the Teacher Coordinator."
+        } else if (note == "unable to enrol") {
+            note = "OBS is unable to enrol the Applicant with the stated condition. Complete this registration and get in touch with the Teacher Coordinator for advice."
+        }
+
+        // Check for the child element with class "insert-note-template"
+        const insertNoteTemplateDiv = callingEl.querySelector('.insert-note-template');
+
+        if (insertNoteTemplateDiv) {
+            while (insertNoteTemplateDiv.firstChild) {
+                insertNoteTemplateDiv.removeChild(insertNoteTemplateDiv.firstChild);
+            }
+
+            if (type) {
+                const content = template.content.cloneNode(true);
+                content.querySelector("h3").textContent = `Note: ${note}`;
+                insertNoteTemplateDiv.appendChild(content);
+            }
+        }
+    }
     // === OPTIONAL DIV HANDLERS ===
 
+    // EMERGENCY CONTACT
     document.querySelectorAll('input[name="emergency-radio"]').forEach(button => {
         button.addEventListener("change", (e) => {
             const targetElement = document.getElementById('emergency-contact-div');
@@ -71,7 +98,8 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     });
 
-    document.querySelectorAll('input[name="emergency-radio"]').forEach(button => {
+    // TETANUS VACCINATION
+    document.querySelectorAll('input[name="tetanus-radio"]').forEach(button => {
         button.addEventListener("change", (e) => {
             const targetElement = document.getElementById('tetanus-date-div');
             const targetElement2 = document.getElementById('tetanus-note-div');
@@ -81,6 +109,238 @@ window.addEventListener('DOMContentLoaded', () => {
             } else if (e.target.value == "1") {
                 targetElement.classList.remove('optional-div');
                 targetElement2.classList.add('optional-div');
+            }
+        })
+    });
+
+    // BREATHING CONDITION
+    document.querySelectorAll('input[name="breathing-condition-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            const targetElement = document.getElementById('breathing-condition-div');
+            if (e.target.value == "0") {
+                targetElement.classList.add('optional-div');
+            } else if (e.target.value == "1") {
+                targetElement.classList.remove('optional-div');
+            }
+        })
+    });
+
+    // HEART CONDITION
+    document.querySelectorAll('input[name="heart-condition-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            const targetElement = document.getElementById('heart-condition-div');
+            if (e.target.value == "0") {
+                targetElement.classList.add('optional-div');
+            } else if (e.target.value == "1") {
+                targetElement.classList.remove('optional-div');
+            }
+        })
+    });
+
+    // BLOOD CONDITION
+    document.querySelectorAll('input[name="blood-condition-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            const targetElement = document.getElementById('blood-condition-div');
+            if (e.target.value == "0") {
+                targetElement.classList.add('optional-div');
+            } else if (e.target.value == "1") {
+                targetElement.classList.remove('optional-div');
+            }
+        })
+    });
+
+    // > CHOOSE BLOOD CONDITION
+    document.querySelectorAll('input[name="blood-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            if (e.target.value == "0" || e.target.value == "3") { // ANAEMIA / OTHERS
+                displayNote(document.getElementById('blood-condition-div'), "", 0)
+                if (document.querySelector('input[name="blood-followup-radio"]:checked') != null &&
+                    document.querySelector('input[name="blood-followup-radio"]:checked').value == 1) {
+                    displayNote(document.getElementById('blood-condition-div'), "specialist memo", 1)
+                }
+                document.getElementById('blood-followup-div').classList.remove('optional-div');
+            } else if (e.target.value == "2") { // T MAJOR
+                displayNote(document.getElementById('blood-condition-div'), "unable to enrol", 1)
+                document.getElementById('blood-followup-div').classList.add('optional-div');
+            } else if (e.target.value == "1") { // T MINOR
+                displayNote(document.getElementById('blood-condition-div'), "", 0)
+                document.getElementById('blood-followup-div').classList.add('optional-div');
+            }
+        })
+    });
+
+    // > > BLOOD CONDITION(OTHERS / ANAEMIA) - FOLLOWUP CONDITION
+    document.querySelectorAll('input[name="blood-followup-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            if (e.target.value == "0") {
+                // remove template
+                displayNote(document.getElementById('blood-condition-div'), "", 0)
+            } else if (e.target.value == "1") {
+                // add template
+                displayNote(document.getElementById('blood-condition-div'), "specialist memo", 1)
+            }
+        })
+    });
+
+    // EPILEPSY, FITS OR SEIZURE CONDITION
+    document.querySelectorAll('input[name="epilepsy-condition-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            const targetElement = document.getElementById('epilepsy-condition-div');
+            if (e.target.value == "0") {
+                targetElement.classList.add('optional-div');
+            } else if (e.target.value == "1") {
+                targetElement.classList.remove('optional-div');
+            }
+        })
+    });
+
+    // > EPILEPSY, FITS OR SEIZURE IN THE PAST 24 MONTHS
+    document.querySelectorAll('input[name="epilepsy-past-months-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            if (e.target.value == "0") {
+                // remove template
+                if (!(document.querySelector('input[name="epilepsy-medication-radio"]:checked') != null &&
+                    document.querySelector('input[name="epilepsy-medication-radio"]:checked').value == 1)) {
+                    displayNote(document.getElementById('epilepsy-condition-div'), "", 0)
+
+                    if (document.querySelector('input[name="epilepsy-medication-radio"]:checked') != null) {
+                        document.getElementById('epilepsy-followup-div').classList.remove('optional-div');
+
+                        if (document.querySelector('input[name="epilepsy-followup-radio"]:checked') != null &&
+                            document.querySelector('input[name="epilepsy-followup-radio"]:checked').value == 1) {
+                            displayNote(document.getElementById('epilepsy-condition-div'), "specialist memo", 1)
+                        }
+                    }
+
+                }
+            } else if (e.target.value == "1") {
+                // add template
+                document.getElementById('epilepsy-followup-div').classList.add('optional-div');
+                displayNote(document.getElementById('epilepsy-condition-div'), "unable to enrol", 1)
+            }
+        })
+    });
+
+    // > EPILEPSY, FITS OR SEIZURE MEDICATION IN THE PAST 24 MONTHS
+    document.querySelectorAll('input[name="epilepsy-medication-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            if (e.target.value == "0") {
+                // remove template
+                if (!(document.querySelector('input[name="epilepsy-past-months-radio"]:checked') != null &&
+                    document.querySelector('input[name="epilepsy-past-months-radio"]:checked').value == 1)) {
+                    displayNote(document.getElementById('epilepsy-condition-div'), "", 0)
+
+                    if (document.querySelector('input[name="epilepsy-past-months-radio"]:checked') != null) {
+                        document.getElementById('epilepsy-followup-div').classList.remove('optional-div');
+
+                        if (document.querySelector('input[name="epilepsy-followup-radio"]:checked') != null &&
+                            document.querySelector('input[name="epilepsy-followup-radio"]:checked').value == 1) {
+                            displayNote(document.getElementById('epilepsy-condition-div'), "specialist memo", 1)
+                        }
+                    }
+
+                }
+            } else if (e.target.value == "1") {
+                // add template
+                document.getElementById('epilepsy-followup-div').classList.add('optional-div');
+                displayNote(document.getElementById('epilepsy-condition-div'), "unable to enrol", 1)
+            }
+        })
+    });
+
+    // > > EPILEPSY, FITS OR SEIZURE - FOLLOWUP CONDITION
+    document.querySelectorAll('input[name="epilepsy-followup-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            if (e.target.value == "0") {
+                // remove template
+                displayNote(document.getElementById('epilepsy-condition-div'), "", 0)
+
+            } else if (e.target.value == "1") {
+                // add template
+                displayNote(document.getElementById('epilepsy-condition-div'), "specialist memo", 1)
+            }
+        })
+    });
+
+    // BONE / JOINT/ TENDON CONDITION
+    document.querySelectorAll('input[name="bone-condition-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            const targetElement = document.getElementById('bone-condition-div');
+            if (e.target.value == "0") {
+                targetElement.classList.add('optional-div');
+            } else if (e.target.value == "1") {
+                targetElement.classList.remove('optional-div');
+            }
+        })
+    });
+
+    // > BONE / JOINT/ TENDON CONDITION - FOLLOW UP CONDITION
+    document.querySelectorAll('input[name="bone-followup-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            if (e.target.value == "0") {
+                // remove template
+                displayNote(document.getElementById('bone-condition-div'), "", 0)
+
+            } else if (e.target.value == "1") {
+                // add template
+                displayNote(document.getElementById('bone-condition-div'), "physiotherapist memo", 1)
+            }
+        })
+    });
+
+    // BEHAVIOURAL OR PSYCHOLOGICAL CONDITION
+    document.querySelectorAll('input[name="behavioural-condition-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            const targetElement = document.getElementById('behavioural-condition-div');
+            if (e.target.value == "0") {
+                targetElement.classList.add('optional-div');
+            } else if (e.target.value == "1") {
+                targetElement.classList.remove('optional-div');
+            }
+        })
+    });
+
+    // > BEHAVIOURAL OR PSYCHOLOGICAL CONDITION - FOLLOW UP CONDITION
+    document.querySelectorAll('input[name="behavioural-followup-radio"]').forEach(button => {
+        button.addEventListener("change", (e) => {
+            const targetElement = document.getElementById('behavioural-specialist-progress-div');
+            if (e.target.value == "0") {
+                targetElement.classList.add('optional-div');
+            } else if (e.target.value == "1") {
+                targetElement.classList.remove('optional-div');
+            }
+        })
+    });
+
+
+
+
+    // RADIO OTHER LABEL EVENT HANDLER
+    document.querySelectorAll('.radio-other-label').forEach(button => {
+        button.addEventListener("click", (e) => {
+            const containerDiv = e.target.closest('.radio-others');
+            if (containerDiv) {
+                // Find the radio button within the container div
+                const radioElement = containerDiv.querySelector('input[type="radio"]');
+                if (radioElement && !radioElement.checked) {
+                    radioElement.checked = true;
+                    radioElement.dispatchEvent(new Event('change'));
+                }
+            }
+        })
+    });
+
+    // RADIO OTHER INPUT EVENT HANDLER
+    document.querySelectorAll('.radio-other-input').forEach(button => {
+        button.addEventListener("input", (e) => {
+            const containerDiv = e.target.closest('.radio-others');
+            if (containerDiv) {
+                // Find the radio button within the container div
+                const radioElement = containerDiv.querySelector('input[type="radio"]');
+                if (radioElement && !radioElement.checked) {
+                    radioElement.checked = true;
+                    radioElement.dispatchEvent(new Event('change'));
+                }
             }
         })
     });
