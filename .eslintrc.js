5cce83f00ae9f6a83683d6088c1448af7c0e1979
Jerry Ng <jerryngsw.20@ichat.sp.edu.sg>
2023-08-02 14:51:17 +0800

    eslint configuration added #23

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: false,
+        commonjs: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ["airbnb-base", "prettier"],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    plugins: [],
+    ignorePatterns: ["node_modules/*", ".env"],
+    overrides: [
+        {
+            files: ["**/*.js"],
+            rules: {
+                'func-names': 'off',
+                'prefer-arrow-callback': 'off',
+                'no-console': 'error',
+                'no-unused-vars': 'off',
+                'no-restricted-globals': 'off',
+                'no-param-reassign': [2, {
+                    "props": false
+                }]
+            },
+        },
+    ],
+};
