60d7885ef417f1c0d3f20bcaff708b7921d61da3
isaacpeh <isaacpeh123@gmail.com>
2023-08-08 18:41:19 +0800

    fixed eslint

--- public/error/index.js
+++ public/error/index.js
@@ -1,54 +1,55 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const params = new URLSearchParams(window.location.search);
-    const actionBtn = document.getElementById('action-button');
-    let code = params.get('code')
-    let errMessage, errText;
+  const params = new URLSearchParams(window.location.search);
+  const actionBtn = document.getElementById('action-button');
+  let code = params.get('code');
+  let errMessage; let
+    errText;
 
-    const url = window.location.search;
-    const searchParams = new URLSearchParams(url);
-    const type = searchParams.get("type");
-    const encrypt = searchParams.get("encrypted");
+  const url = window.location.search;
+  const searchParams = new URLSearchParams(url);
+  const type = searchParams.get('type');
+  const encrypt = searchParams.get('encrypted');
 
-    const actionBtnHandler = (text, display) => {
-        actionBtn.textContent = text;
-        actionBtn.style.display = display;
+  const actionBtnHandler = (text, display) => {
+    actionBtn.textContent = text;
+    actionBtn.style.display = display;
 
-        actionBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            if (code == 401) {
-                if (encrypt && type) {
-                    window.location.replace(`/${type}/?encrypted=${encrypt}`)
-                } else if (!encrypt && type) {
-                    window.location.replace(`/${type}/login`)
-                } else {
-                    window.location.replace(`/login`)
-                }
-            } else {
-                history.back();
-            }
-        })
-    }
+    actionBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (code === 401) {
+        if (encrypt && type) {
+          window.location.replace(`/${type}/?encrypted=${encrypt}`);
+        } else if (!encrypt && type) {
+          window.location.replace(`/${type}/login`);
+        } else {
+          window.location.replace('/login');
+        }
+      } else {
+        history.back();
+      }
+    });
+  };
 
-    if (code == 404) {
-        errMessage = 'Resource Not Found'
-        errText = 'Oops! The requested resource could not be found. Please check the URL and try again.'
-        actionBtnHandler('', 'none');
-    } else if (code == 401) {
-        errMessage = 'Unauthorized Access'
-        errText = 'You are unauthorized or may have expired access the requested resource. Please re-login and try again.'
-        actionBtnHandler('Login', 'block');
-    } else if (code == 403) {
-        errMessage = 'Access Forbidden'
-        errText = 'Access to the requested resource is forbidden. You do not have the necessary permissions to access this page.'
-        actionBtnHandler('Go back', 'block');
-    } else {
-        code = 500
-        errMessage = 'Internal Server Error'
-        errText = 'An internal server error occurred. Our team has been notified of the issue. Please try again later.'
-        actionBtnHandler('Go back', 'block');
-    }
+  if (code === 404) {
+    errMessage = 'Resource Not Found';
+    errText = 'Oops! The requested resource could not be found. Please check the URL and try again.';
+    actionBtnHandler('', 'none');
+  } else if (code === 401) {
+    errMessage = 'Unauthorized Access';
+    errText = 'You are unauthorized or may have expired access the requested resource. Please re-login and try again.';
+    actionBtnHandler('Login', 'block');
+  } else if (code === 403) {
+    errMessage = 'Access Forbidden';
+    errText = 'Access to the requested resource is forbidden. You do not have the necessary permissions to access this page.';
+    actionBtnHandler('Go back', 'block');
+  } else {
+    code = 500;
+    errMessage = 'Internal Server Error';
+    errText = 'An internal server error occurred. Our team has been notified of the issue. Please try again later.';
+    actionBtnHandler('Go back', 'block');
+  }
 
-    document.getElementById('err-message').textContent = errMessage;
-    document.getElementById('err-text').textContent = errText;
-    document.getElementById('err-code').textContent = code;
-})
+  document.getElementById('err-message').textContent = errMessage;
+  document.getElementById('err-text').textContent = errText;
+  document.getElementById('err-code').textContent = code;
+});
--- public/obs-admin/login/index.js
+++ public/obs-admin/login/index.js
@@ -1,78 +1,71 @@
 window.addEventListener('DOMContentLoaded', () => {
+  const loginForm = this.document.querySelector('#login-form');
+  const errMsg = document.getElementById('err-message');
+  const errMsgContainer = document.getElementById('err-message-container');
 
-    const loginForm = this.document.querySelector("#login-form");
-    const errMsg = document.getElementById('err-message');
-    const errMsgContainer = document.getElementById('err-message-container');
+  const setErrMsg = (message, toggle) => {
+    errMsg.innerHTML = `<i class="material-symbols-outlined">error</i>${message}`;
+    errMsgContainer.style.visibility = toggle;
+  };
 
-    const login = (email, password) => {
+  const login = (email, password) => fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
 
-        return fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email,
-                password
-            })
-
-        }).then((response) => {
-
-            if (response.status === 400 || response.status === 401 || response.status === 404) {
-                const error = new Error("The email or password you entered is incorrect.");
-                error.status = response.status;
-                throw error;
-            }
-
-            if (response.status === 403) {
-                const error = new Error("Account is disabled. Please contact admin for more information.");
-                error.status = response.status;
-                throw error;
-            }
-
-            if (response.redirected) {
-                window.location.href = response.url
-                throw new Error('redirected');
-
-            } else {
-                const error = new Error("An error occured logging in.");
-                error.status = response.status;
-                throw error;
-            }
-
-        }).catch((error) => {
-            if (error && error.message != 'redirected') {
-                setErrMsg(error.message, 'visible');
-            }
-        }).finally(() => {
-            document.querySelectorAll('.spinner-item').forEach(i => {
-                i.style.display = 'none';
-            });
-            document.getElementById('login-text').style.display = 'inline-block';
-            document.getElementById('login-button').style.backgroundColor = '#485EAB';
-        })
+  }).then((response) => {
+    if (response.status === 400 || response.status === 401 || response.status === 404) {
+      const error = new Error('The email or password you entered is incorrect.');
+      error.status = response.status;
+      throw error;
     }
 
-    loginForm.onsubmit = (e) => {
-        e.preventDefault();
-
-        const email = loginForm.querySelector('#login-email').value.trim();
-        const password = loginForm.querySelector('#login-password').value;
-
-        if (!email || !password) {
-            setErrMsg('Please fill in all fields.', 'visible');
-        } else {
-            document.querySelectorAll('.spinner-item').forEach(i => {
-                i.style.display = 'inline-block';
-            });
-            document.getElementById('login-text').style.display = 'none';
-            document.getElementById('login-button').style.backgroundColor = '#4d4d4d';
-            login(email, password)
-        }
-    };
-
-    const setErrMsg = (message, toggle) => {
-        errMsg.innerHTML = `<i class="material-symbols-outlined">error</i>${message}`;
-        errMsgContainer.style.visibility = toggle;
+    if (response.status === 403) {
+      const error = new Error('Account is disabled. Please contact admin for more information.');
+      error.status = response.status;
+      throw error;
     }
-})
+
+    if (response.redirected) {
+      window.location.href = response.url;
+      throw new Error('redirected');
+    } else {
+      const error = new Error('An error occured logging in.');
+      error.status = response.status;
+      throw error;
+    }
+  }).catch((error) => {
+    if (error && error.message !== 'redirected') {
+      setErrMsg(error.message, 'visible');
+    }
+  }).finally(() => {
+    document.querySelectorAll('.spinner-item').forEach((i) => {
+      i.style.display = 'none';
+    });
+    document.getElementById('login-text').style.display = 'inline-block';
+    document.getElementById('login-button').style.backgroundColor = '#485EAB';
+  });
+
+  loginForm.onsubmit = (e) => {
+    e.preventDefault();
+
+    const email = loginForm.querySelector('#login-email').value.trim();
+    const password = loginForm.querySelector('#login-password').value;
+
+    if (!email || !password) {
+      setErrMsg('Please fill in all fields.', 'visible');
+    } else {
+      document.querySelectorAll('.spinner-item').forEach((i) => {
+        i.style.display = 'inline-block';
+      });
+      document.getElementById('login-text').style.display = 'none';
+      document.getElementById('login-button').style.backgroundColor = '#4d4d4d';
+      login(email, password);
+    }
+  };
+});
