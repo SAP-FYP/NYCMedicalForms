d7a932c40cad70b123d3805d41acbbe27024f518
Jerry Ng <jerryngsw.20@ichat.sp.edu.sg>
2023-08-08 16:40:27 +0800

    Eslint Configuration #23

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    extends: ["airbnb-base", "prettier"],
+    extends: ["airbnb-base"],
     parserOptions: {
         ecmaVersion: 12,
     },
