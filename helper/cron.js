a4ea29873e2b251900105369d46827baf7f539e7
barry <leebarry008@gmail.com>
2023-08-11 12:28:39 +0800

    Added OBS logo into the cron #7

--- helper/cron.js
+++ helper/cron.js
@@ -73,22 +73,79 @@ module.exports.remindParentJob = function remindParentJob() {
             result.forEach(async (parent) => {
                 const email = parent.parentEmail;
 
-                const htmlString = `
-                <!DOCTYPE html>
+                const htmlString = `<!DOCTYPE html>
                 <html lang="en">
                 <head>
                     <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
                     <title>Reminder: Important Medical Updates</title>
+                    <style>
+                        body {
+                            font-family: Arial, sans-serif;
+                            padding: 20px;
+                            background-color: #f5f5f5;
+                        }
+                
+                        .container {
+                            max-width: 600px;
+                            margin: 0 auto;
+                            background-color: #fff;
+                            padding: 20px;
+                            border-radius: 5px;
+                            box-shadow: 0 2px 5px rgba(0, 0, 0, .1);
+                        }
+                
+                        .logo {
+                            text-align: left;
+                            margin-bottom: 40px;
+                        }
+                
+                        .logo img {
+                            max-width: 300px;
+                        }
+                
+                        .message {
+                            margin-bottom: 20px;
+                            font-size: 16px;
+                        }
+                
+                        .footer {
+                            text-align: center;
+                            color: #888;
+                            font-size: 14px;
+                            margin-top: 20px;
+                            border-top: 1px solid #ccc;
+                            padding-top: 20px;
+                        }
+                
+                        .footer hr {
+                            margin-bottom: 10px;
+                        }
+                
+                        .footer p {
+                            margin-bottom: 10px;
+                        }
+                    </style>
                 </head>
                 <body>
-                    <p>Dear Parent,</p>
-                    <p>We hope this email finds you well. This is a friendly reminder that we have recently made important updates to your child's medical records. However, we have not received your acknowledgment of these updates.</p>
-                    <p>It's important for us to have accurate and up-to-date information to ensure the well-being of your child.</p>
-                    <p>If you have already acknowledged the updates, please accept our apologies and disregard this reminder.</p>
-                    <p>Thank you for your prompt attention to this matter.</p>
-                    <p>Warm regards,</p>
-                    <p>National Youth Council in affiliation with Outward Bound Singapore</p>
+                    <div class="container">
+                        <div class="logo">
+                            <img src="https://res.cloudinary.com/sp-esde-2100030/image/upload/v1688051640/obs-logo_pi70gy.png" alt="Logo">
+                        </div>
+                        <div class="message">
+                            <p>Dear Parent,</p>
+                            <p>We hope this email finds you well. We would like to remind you that we have recently made important updates to your child's medical records, but we have not yet received your acknowledgment of these updates.</p>
+                            <p>Your prompt acknowledgment is crucial for us to ensure the well-being of your child.</p>
+                            <p>If you have already acknowledged the updates, please accept our apologies for this reminder.</p>
+                            <p>Thank you for your attention to this matter.</p>
+                            <p>Warm regards,</p>
+                            <p>National Youth Council in affiliation with Outward Bound Singapore</p>
+                        </div>
+                        <div class="footer">
+                            <p>This email was sent to you by the Administrative Team. If you have any questions, please contact our support team.</p>
+                            <p>© National Youth Council | Outward Bound Singapore.</p>
+                        </div>
+                    </div>
                 </body>
                 </html>
                 `
